66bc4ce54cd6eb0d2a461e3e04281a8ecdcfec59
Olena <kondratchukov@gmail.com>
2024-07-30 13:41:56 +0200

    fixed_baseUrl

--- src/js/baseUrl.js
+++ src/js/baseUrl.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   if (window.location.hostname.endsWith('github.io')) {
-    const baseURL = 'https://feraxoft.github.io/feraxoft/';
+    const baseURL = '/feraxoft/';
     const base = document.createElement('base');
     base.setAttribute('href', baseURL);
     document.head.appendChild(base);
